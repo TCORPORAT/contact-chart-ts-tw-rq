e6272d3d6be89f66b2599721207698fd0d285ab8
Tanmay D <tanmay@icubes.org>
2023-05-16 22:18:21 +0530

    Chart Update

--- src/pages/Chart.tsx
+++ src/pages/Chart.tsx
@@ -9,9 +9,6 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import axios from 'axios';
 
 const Chart = () => {
-  // const position = [51.505, -0.09];
-  const position = [0, 0];
-
   const { isLoading, error, data, isFetching } = useQuery({
     queryKey: ['countryList'],
     queryFn: () =>
@@ -24,22 +21,17 @@ const Chart = () => {
 
   if (error) return 'An error has occurred: ' + error.message;
 
-
-  let latLongList:Array<Array<any>> = [];
   const countryLists = data.map((item: any) => {
-    latLongList.push([item.countryInfo.lat, item.countryInfo.long]);
     return {
       name: item.country,
-      latlong : [item.countryInfo.lat, item.countryInfo.long],
+      latlong: [item.countryInfo.lat, item.countryInfo.long],
       cases: item.active,
       deaths: item.deaths,
-      recovered: item.recovered
+      recovered: item.recovered,
     };
-});
+  });
 
-
- console.log(latLongList);
-
+  // console.log(latLongList);
 
   return (
     <DefaultLayout>
@@ -48,11 +40,10 @@ const Chart = () => {
       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
         <div className="col-span-12">
           <ChartOne />
-          {/* <ChartFour /> */}
         </div>
 
         <MapContainer
-          center={0,0}
+          center={[0, 0]}
           zoom={0}
           scrollWheelZoom={true}
           style={{ minHeight: '500px', minWidth: '1200px' }}
@@ -61,20 +52,20 @@ const Chart = () => {
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-   {countryLists.map((item: any) => {
-          <Marker position={[latLongList]}>
-            <Popup>
-              Country :{item.name}
-              {/* Active Cases : {item.cases}
-              Recovered :{item.recovered}
-              Deaths : {item.deaths} */}
-            </Popup>
-          </Marker>
-        })}
+          {countryLists.map((pos: string | number | Array, index: number) => {
+            return (
+              <Marker position={pos.latlong} key={index}>
+                <Popup>{`
+              Country : ${pos.name} \n
+              Total Cases : ${pos.cases}\n
+               Total Recovered : ${pos.recovered}\n
+               Deaths: ${pos.deaths}   
+              `}</Popup>
+              </Marker>
+            );
+          })}
+          ;
         </MapContainer>
-        {/* 
-        <ChartTwo />
-        <ChartThree /> */}
       </div>
     </DefaultLayout>
   );
